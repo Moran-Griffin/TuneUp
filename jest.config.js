21a6c6f16a646d744eca54c4c6491706132b052e
Moran-Griffin <moran2gc@dukes.jmu.edu>
2026-03-29 21:20:08 -0400

    fix: correct expo-router entrypoint, jest config key, and add test script

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFilesAfterFramework: ['@testing-library/react-native/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 };
